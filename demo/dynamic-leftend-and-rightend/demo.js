ce5ccfa9cd3ab9a6a3856c4e05ae230d2676af2a
shuizhongyueming <shuizhongyueming@gmail.com>
2015-07-23 18:31:23 +0800

    修正依赖的文件的路径;调整两侧四个按钮是否高亮的展示

--- demo/dynamic-leftend-and-rightend/demo.js
+++ demo/dynamic-leftend-and-rightend/demo.js
@@ -2,8 +2,8 @@
 require.config(
     {
         paths: {
-            'jquery': ['./lib/jquery.min'],
-            'paging': './paging',
+            'jquery': ['../../lib/jquery.min'],
+            'paging': '../../paging',
         },
         waitSeconds: 90
     }
@@ -45,6 +45,22 @@ require(['jquery', 'paging'], function($, Paging){
             }
 
             return '<span onselectstart="return false;" unselectable="on">...'+me.maxPage+'</span>';
+        }
+    };
+
+    paging.makeNextBtn = function(isActive) {
+        if (isActive) {
+            return '<span onselectstart="return false;" unselectable="on">下一页</span>';
+        } else {
+            return '<span onselectstart="return false;" unselectable="on" class="active">下一页</span>';
+        }
+    };
+
+    paging.makePrevBtn = function(isActive) {
+        if (isActive) {
+            return '<span onselectstart="return false;" unselectable="on">上一页</span>';
+        } else {
+            return '<span onselectstart="return false;" unselectable="on" class="active">上一页</span>';
         }
     };
     paging.init({
